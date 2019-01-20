c23cf855cbd644038fab371c3439537624c8b5c6
tom <thomasmaltese64@gmail.com>
2019-01-19 22:18:50 -0500

    add support for --long options

--- argv.h
+++ argv.h
@@ -7,26 +7,33 @@
 #ifndef ARGV_H__
 #define ARGV_H__
 
-#define ARGBEGIN \
-int ac; \
-char **av; \
-for (ac = argc - 1, av = argv + 1; \
-     ac && *av && av[0][0] == '-'; ac--, av++) { \
+#define LONGARG \
+for (argc--, argv++; \
+     argc && argv[0][0] == '-'; argc--, argv++) { \
 	int brk = 0; \
-	char *op; \
-	if (av[0][1] == '-') { \
-		ac--; av++; \
-		break; \
+	char *op, *option; \
+	if (argv[0][1] == '-') { \
+		if (argv[0][2] == '\0') { \
+			argc--; argv++; \
+			break; \
+		} \
+		option = argv[0] + 2; \
+		(void)option;
+
+#define SHORTARG \
 	} \
-	for (op = (*av) + 1; *op && !brk; op++) { \
+	for (op = (*argv) + 1; *op && !brk; op++) { \
 		switch (*op)
 #define ARGEND \
 	}\
 }
 
-#define GETF() \
+#define GETL() \
+(argv[1] && argv[1][0] != '-') ? (brk = 1, --argc, *(++argv)) : (char*)0x0
+
+#define GETS() \
 (!brk && op[1] != '\0') ? (brk = 1, (op + 1)) : \
-(av[1] && av[1][0] != '-') ? (brk = 1, --ac, *(++av)) : \
+(argv[1] && argv[1][0] != '-') ? (brk = 1, --argc, *(++argv)) : \
                              (brk = 1, (char*)0x0)
 
 #endif  /* ARGV_H__ */
--- example.c
+++ example.c
@@ -1,20 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "argv.h"
+
+void usage()
+{
+	printf(
+"usage: a.out [-hcd] [-x STR...] [-f FILE...] [--foo STR] [--bar STR] [--baz STR...]\n"
+	);
+}
 
 int main(int argc, char **argv)
 {
 	char *tmp;
-	ARGBEGIN {
+	LONGARG {
+	if (strcmp(option, "foo") == 0) {
+		tmp = GETL();
+		printf("foo = %s\n", tmp);
+	} else if (strcmp(option, "bar") == 0) {
+		tmp = GETL();
+		printf("bar = %s\n", tmp);
+	} else if (strcmp(option, "baz") == 0) {
+		printf("baz:");
+		while (tmp = GETL()) {
+			printf(" %s", tmp);
+		}
+		putchar('\n');
+	} else {
+		printf("Unkown option: %s\n", option);
+		return 1;
+	}
+
+	} SHORTARG {
 	case 'x':
 		printf("x values:");
-		while (tmp = GETF()) {
+		while (tmp = GETS()) {
 			printf(" %s", tmp);
 		}
 		putchar('\n');
 		break;
 	case 'f':
 		printf("f values:");
-		while (tmp = GETF()) {
+		while (tmp = GETS()) {
 			printf(" %s", tmp);
 		}
 		putchar('\n');
@@ -22,9 +49,22 @@ int main(int argc, char **argv)
 	case 'c':
 		printf("received -c option\n");
 		break;
+	case 'd':
+		printf("Received -d option\n");
+		break;
+	case 'h':
+		usage();
+		return 0;
 	default:
 		printf("Unknown option: %s\n", op);
 		return 0;
-	} ARGEND;
+	} ARGEND
+
+	if (argc) {
+		printf("remaining arguments: %d\n", argc);
+		for (int i = 0; i < argc; i++) {
+			printf("%s\n", argv[i]);
+		}
+	}
 	return 0;
 }
